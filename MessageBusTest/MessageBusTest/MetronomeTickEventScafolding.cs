9034e0e6e40355d387e592f835a5b6e0ce779f9e
alinddicu <alinddicu@yahoo.com>
2014-11-17 16:57:45 +0100

    Event scaffolding

---
+++ MessageBusTest/MessageBusTest/MetronomeTickEventScafolding.cs
@@ -0,0 +1,65 @@
+﻿namespace MessageBusTest
+{
+    using System;
+    using System.Threading;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    // http://www.codeproject.com/Articles/11541/The-Simplest-C-Events-Example-Imaginable
+    public class MetronomeTickEventScafolding
+    {
+        [TestMethod]
+        public void ViewMetronomeEventTickPropagation()
+        {
+            Metronome m = new Metronome();
+            Listener l = new Listener();
+            l.Subscribe(m);
+            m.Start();
+        }
+    }
+
+    public class MetronomeTickArgs : EventArgs 
+    {
+        public MetronomeTickArgs(int count)
+        {
+            Count = count;
+        }
+
+        public int Count { get; set; }
+    }
+
+    public class Metronome
+    {
+        public event TickHandler Tick;
+
+        public delegate void TickHandler(Metronome m, MetronomeTickArgs e);
+
+        public void Start()
+        {
+            var args = new MetronomeTickArgs(0);
+            while (true)
+            {
+                Thread.Sleep(3000);
+                if (Tick != null)
+                {
+                    args.Count++;
+                    Tick(this,  args);
+                }
+            }
+        }
+    }
+
+    public class Listener
+    {
+        public void Subscribe(Metronome m)
+        {
+            m.Tick += new Metronome.TickHandler(HeardIt);
+        }
+
+        private void HeardIt(Metronome m, MetronomeTickArgs e)
+        {
+            var message = string.Format("HEARD IT! {0} times", e.Count);
+            System.Diagnostics.Trace.Write(message + Environment.NewLine);
+        }
+    }
+}
